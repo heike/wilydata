84a42283a00146df324845927bba3ba9ffb04bce
Heike Hofmann <hofmann@iastate.edu>
2018-08-16 18:03:11 -0500

    added acces date to data

--- R/data.R
+++ R/data.R
@@ -2,7 +2,7 @@
 #' Station information for the Scripps stations
 #'
 #' Information on all Scripps stations active on Aug 15 2018.
-#' @format A data frame with 10 rows and 6 variables:
+#' @format A data frame with 10 rows and 8 variables:
 #' \describe{
 #'   \item{Station Name}{name of the station and country/state.}
 #'   \item{Station Code}{three letter station abbreviation. }
@@ -11,6 +11,7 @@
 #'   \item{Elevation}{elevation above sea level in meters.}
 #'   \item{Dates}{Period of active status.}
 #'   \item{Page}{URL of station information.}
+#'   \item{Accessed}{Date last accessed.}
 #' }
 #' @keywords data
 "scripps_stations"
@@ -18,7 +19,7 @@
 #' CO2 daily flask data from Scripps stations
 #'
 #' CO2 flask data of all active Scripps stations, downloaded on Aug 15 2018.
-#' @format A data frame with 12868 rows and 8 variables:
+#' @format A data frame with 12868 rows and 9 variables:
 #' \describe{
 #'   \item{date}{Date the sample was taken.}
 #'   \item{time}{(hms) Time the sample was taken. }
@@ -28,6 +29,7 @@
 #'   \item{flag}{Quality flag for the sample: 0 is accepted, negative values are also accepted but with conditions, positive values indicate abnormalities and are rejected.}
 #'   \item{co2}{CO2 concentration, measured on the '08A' Calibration Scale.}                   ".}
 #'   \item{station_id}{Three letter abbreviation for the station.}
+#'   \item{accessed}{Date last accessed.}
 #' }
 #' @keywords data
 "scripps_data_co2"
